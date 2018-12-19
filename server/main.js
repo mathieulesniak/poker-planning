2aed774bb4e3585fd362a301953ee264aa067b81
Mathieu LESNIAK <mathieu@bureauxapartager.com>
2018-12-19 14:03:29 +0100

    socket wip

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3333;
 const app = new express()
 
 const clients = {};
+const rooms = {};
 
 app.use(cors())
 
@@ -17,16 +18,12 @@ server.listen(port, function(){
 
 
 io.on('connection', function(socket){
-  clients[socket.id] = { socket, nick: ""};
+  clients[socket.id] = { nick: "toto" + socket.id, room: "", socket};
   console.log(`${Object.keys(clients).length} clients connected`)
 
-  socket.on('chat', function(msg){
-    console.log('RECEIVED chatmessage on ' , socket.id);
-    io.emit('chat message', msg);
-  });
-
-  socket.on('SET_NICK', function(msg) {
-
+  socket.on('MESSAGE', function(msg){
+    console.debug('RECEIVED MESSAGE on ' , socket.id, msg);
+    handleMessages(msg, socket);
   });
 
   socket.on('disconnect', () => {
@@ -35,4 +32,55 @@ io.on('connection', function(socket){
   })
 });
 
+function handleMessages(message, socket) {
+  switch (message.type) {
+    case 'join':
+      const roomName= message.payload;
+      clients[socket.id].room = roomName;
+      if (rooms[roomName] === undefined) {
+        rooms[roomName] = {
+          name: roomName,
+          members: [],
+          status: "idle",
+        }
+      }
+      // FIXME: do not add if already member
+      rooms[roomName].members.push(socket.id);
+      ack(socket, message.type, roomName);
+      broadcastMembersList(socket, roomName)
+      break;
+  }
+}
 
+function ack(socket, messageType, payload) {
+  socket.emit('MESSAGE', {
+    type: 'ACK',
+    sourceType: messageType,
+    sourcePayload: payload
+
+  });
+}
+
+function broadcastMembersList(socket, roomName) {
+  console.log(rooms[roomName]);
+  const room = rooms[roomName];
+  if (room.members.length > 1) {
+    room.members.forEach((member) => {
+      clients[member].socket.emit(
+        'MESSAGE',
+        {
+          type: 'MEMBERS_LIST',
+          payload: "sending members list broadcast" + socket.id
+        }
+      )
+    })
+  }
+
+}
+
+function getMemberNameFromSocket(socketId) {
+  return clients[socketId].name
+}
+function removeClientFromRoom(client) {
+
+}
